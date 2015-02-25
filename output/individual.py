30435aa3bdf45ada57757a9abb01524e13cdfa07
malramsay64 <malramsay64@gmail.com>
2015-02-25 18:44:09 +1100

    Output files capable of taking list of molecules
    Currently compiling data on all the molecules in the directory, can take
    a list of only those that are required as a list of arguments after the
    prefix.

--- output/individual.py
+++ output/individual.py
@@ -2,10 +2,19 @@ import os
 import glob
 import sys
 
-prefix = sys.argv[1]
+if len(sys.argv) == 2:
+    prefix = sys.argv[1]
+    dirs = glob.glob("{prefix}/*-*-*".format(prefix=prefix))
+    dirs.sort()
+elif len(sys.argv) > 2:
+    prefix = sys.argv[1]
+    dirs = sys.argv[2:]
+else:
+    prefix = ".."
+    dirs = glob.glob("{prefix}/*-*-*".format(prefix=prefix))
+    dirs.sort()
 
-dirs = glob.glob("{prefix}/*-*-*".format(prefix=prefix))
-dirs.sort()
+
 for dir in dirs:
     f = open("{dir}/mol_pres.tex".format(dir=dir), "rU")
     print f.read()
