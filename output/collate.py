f7de602929853b40919a10a5bb52dcb7079a3a31
malramsay64 <malramsay64@gmail.com>
2015-05-07 22:06:23 +1000

    Exclude all molecular from initial collated section

--- output/collate.py
+++ output/collate.py
@@ -9,7 +9,7 @@ if len(sys.argv) >= 3:
     prefix = sys.argv[1]
     mol = sys.argv[2]
     if len(sys.argv) > 3:
-        exclude=sys.argv[3:]
+        exclude=["Snowman", "Trimer"]
         create.collatedEx("{prefix}/plots/".format(prefix=prefix), mol+"*.png", "Temperature Dependence", exclude)
     else:
         create.collated("{prefix}/plots/".format(prefix=prefix), mol+"*.png", "Temperature Dependence")
